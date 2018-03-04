36985c5e45799467b40408fa2230a5068d7f47ef
Mauricio Pereira <josemauriciobpfilho@gmail.com>
2018-03-03 23:43:21 -0300

    alterando aplicação

--- src/br/unifor/unifor_bank/App.kt
+++ src/br/unifor/unifor_bank/App.kt
@@ -1,49 +1,19 @@
 package br.unifor.unifor_bank
 
-import br.unifor.unifor_bank.entidades.Agencia
-import br.unifor.unifor_bank.entidades.ContaCorrente
-import br.unifor.unifor_bank.entidades.PessoaFisica
-import br.unifor.unifor_bank.servico.Saldo
-import br.unifor.unifor_bank.servico.Saque
+import br.unifor.unifor_bank.entidades.*
+import br.unifor.unifor_bank.servico.*
 
 fun main(args: Array<String>) {
     val agencia = Agencia(1, "BB", 1234)
     val correntista = PessoaFisica(1, "Jose", "jose@email.com", "default", "03739802308")
     val conta = ContaCorrente(1, agencia, correntista, 123456, 100.00, 100.00)
+    val contaPoupanca = ContaPoupanca(3, agencia, correntista, 456789, 50.00)
 
+    val agencia2 = Agencia(2, "BB", 5678)
+    val correntista2 = PessoaFisica(2, "Paulo", "paulo@email.com", "default", "03456702406")
+    val conta2 = ContaCorrente(2, agencia2, correntista2, 234567, 100.00, 100.00)
+    val contaSalario = ContaSalario(4, agencia2, correntista2, 456789, 50.00)
 
-    aplicacao(agencia, conta)
-
-}
-
-fun aplicacao(agencia :Agencia, conta: ContaCorrente){
-    println("Olá, seja bem vindo ao BB!")
-    println("Por favor, informe o numero da sua âgencia:")
-    val numeroAgencia = readLine()?.toInt()
-    println("Informe o numero da sua conta:")
-    val numeroConta = readLine()?.toInt()
-    if (numeroAgencia == agencia.numero && numeroConta == conta.numero){
-        println("Informe sua senha:")
-        val senha = readLine()
-        if (senha == conta.cliente.senha){
-            println("Selecione entre as seguintes opções: 1 - Saque, 2 - Tranferencia, 3 - Deposito, 4 - Saldo")
-            val opcao = readLine()?.toInt()
-            when (opcao){
-                1 -> {
-                    println("Informe o valor que deseja sacar:")
-                    val valorSacado = readLine()?.toDouble()
-                    if (valorSacado != null){
-                        val saque = Saque(conta, valorSacado)
-                        saque.executar()
-                    }else{
-                        println("Erro na operação!")
-                    }
-                }
-            }
-        }else{
-            println("Dados incorretos!")
-        }
-    }else{
-        println("Conta não encontrada!")
-    }
+    val atm = ATM()
+    atm.executarATM(agencia, conta, conta2)
 }
--- src/br/unifor/unifor_bank/servico/ATM.kt
+++ src/br/unifor/unifor_bank/servico/ATM.kt
@@ -1,10 +1,95 @@
 package br.unifor.unifor_bank.servico
 
+import br.unifor.unifor_bank.entidades.Agencia
+import br.unifor.unifor_bank.entidades.Conta
+
 class ATM {
+    fun executarATM(agencia : Agencia, conta: Conta, conta2: Conta) {
+        var x = 0
+        println("Olá, seja bem vindo ao BB!")
+        do{
+            println("Por favor, informe o numero da sua âgencia:")
+            val numeroAgencia = readLine()?.toInt()
+            println("Informe o numero da sua conta:")
+            val numeroConta = readLine()?.toInt()
+            if (numeroAgencia == agencia.numero && numeroConta == conta.numero){
+                println("Informe sua senha:")
+                val senha = readLine()
+                if (senha == conta.cliente.senha){
+                    do {
+                        println("Selecione entre as seguintes opções: 1 - Saque, 2 - Tranferencia, 3 - Deposito, 4 - Saldo, 0 - sair")
+                        val opcao = readLine()?.toInt()
+                        var y = 0
+                        when (opcao){
+                            1 -> {
+                                println("Informe o valor que deseja sacar:")
+                                val valorSacado = readLine()?.toDouble()
+                                if (valorSacado != null){
+                                    val saque = Saque(conta, valorSacado)
+                                    saque.executar()
+                                    x = 5
+                                    y = 5
+                                }else{
+                                    println("Erro na operação!")
+                                    println("Tente novamente!")
+                                    x = 5
+                                    y = 5
+                                }
+                            }
+                            2 -> {
+                                println("Informe o valor da transferencia:")
+                                val valorTransferencia = readLine()?.toDouble()
+                                if (valorTransferencia != null){
+                                    val transferencia = Transferencia(conta, conta2, valorTransferencia)
+                                    transferencia.executar()
+                                    x = 5
+                                    y = 5
+                                }else{
+                                    println("Erro na operação!")
+                                    println("Tente novamente!")
+                                    x = 5
+                                    y = 5
+                                }
+                            }
+                            3 -> {
+                                println("Informe o valor à depositar:")
+                                val valorDeposito = readLine()?.toDouble()
+                                if (valorDeposito != null){
+                                    val deposito = Deposito(conta, valorDeposito)
+                                    deposito.executar()
+                                    x = 5
+                                    y = 5
+                                }else{
+                                    println("Erro na operação!")
+                                    println("Tente novamente!")
+                                    x = 5
+                                    y = 5
+                                }
+                            }
+                            4 -> {
+                                val saldo = Saldo(conta)
+                                saldo.executar()
+                                x = 5
+                                y = 5
+                            }
+                            0 -> {
+                                x = 0
+                                y = 0
+                            }
+                        }
+                    }while (y != 0)
+                }else{
+                    println("Dados incorretos!")
+                    println("Tente novamente!")
+                    x = 1
+                }
+            }else{
+                println("Conta não encontrada!")
+                println("Tente novamente!")
+                x = 1
+            }
+        }while (x != 0)
 
-
-    fun executarServico(servico: Servico) {
-
+        println("Bye, até a próxima!")
     }
-
 }
